1f74584182537b574d1e474ab99ac49851366a30
Jake <jwsmit24@asu.edu>
2024-02-09 20:43:25 -0600

    Continue build.gradle setup and add a basic Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Basic main class to ensure everything is set up properly.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Dungeon Descent!");
+    }
+}
