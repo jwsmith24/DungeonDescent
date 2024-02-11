f06f2276f34f2173e766e64b1c4362869b9c0b28
Jake <jwsmit24@asu.edu>
2024-02-10 18:47:59 -0600

    Make checkstyle corrections on BasicCharacter.java

--- src/main/java/character/BasicCharacter.java
+++ src/main/java/character/BasicCharacter.java
@@ -1,62 +1,64 @@
 package character;
 
 /**
- * Abstract class to define the base player character before its decorated by class, race, and background modifiers.
+ * Abstract class to define the base player character
+ * before its decorated by class, race, and background modifiers.
  */
 public class BasicCharacter implements Character {
 
-   // Core Stats
-   int attack;
-   int defence;
-   int hitPoints;
-   int energy;
-   int speed;
-   int luck;
 
-   // Skills
-   int dungeoneering;
-   int lockPicking;
-   int athletics;
-   int arcana;
-   int history;
+    // Core Stats
+    int attack;
+    int defence;
+    int hitPoints;
+    int energy;
+    int speed;
+    int luck;
 
-   // Player Info
-   String name;
-   String race;
-   int experience;
-   String playerClass;
-   String specialAbility;
-   String racialAbility;
-   String attackType;
+    // Skills
+    int dungeoneering;
+    int lockPicking;
+    int athletics;
+    int arcana;
+    int history;
+
+    // Player Info
+    String name;
+    String race;
+    int experience;
+    String playerClass;
+    String specialAbility;
+    String racialAbility;
+    String attackType;
 
 
     /**
      * A Player starts with all the base stats plus whatever they're named by the user.
      * @param name character name
      */
-   public BasicCharacter(String name) {
+    public BasicCharacter(String name) {
 
-       // Set base stats
-       this.attack = GameConstants.BASE_ATTACK;
-       this.defence = GameConstants.BASE_DEF;
-       this.hitPoints = GameConstants.BASE_HP;
-       this.energy = GameConstants.BASE_ENERGY;
-       this.speed = GameConstants.BASE_SPEED;
+        // Set base stats
+        this.attack = GameConstants.BASE_ATTACK;
+        this.defence = GameConstants.BASE_DEF;
+        this.hitPoints = GameConstants.BASE_HP;
+        this.energy = GameConstants.BASE_ENERGY;
+        this.speed = GameConstants.BASE_SPEED;
 
-       // Set base skills
-       this.dungeoneering = GameConstants.BASE_DUNGEONEERING;
-       this.lockPicking = GameConstants.BASE_LOCKPICKING;
-       this.athletics = GameConstants.BASE_ATHLETICS;
-       this.arcana = GameConstants.BASE_ARCANA;
-       this.history = GameConstants.BASE_HISTORY;
+        // Set base skills
+        this.dungeoneering = GameConstants.BASE_DUNGEONEERING;
+        this.lockPicking = GameConstants.BASE_LOCKPICKING;
+        this.athletics = GameConstants.BASE_ATHLETICS;
+        this.arcana = GameConstants.BASE_ARCANA;
+        this.history = GameConstants.BASE_HISTORY;
 
-       // Set base player info
-       this.name = name;
-       this.playerClass = GameConstants.CLASSLESS;
-       this.attackType = GameConstants.UNARMED_ATTACK;
-       this.experience = 0;
-       this.race = null;
-   }
+        // Set base player info
+        this.name = name;
+        this.playerClass = GameConstants.CLASSLESS;
+        this.attackType = GameConstants.UNARMED_ATTACK;
+        this.experience = 0;
+        this.race = null;
+    }
 
 
     @Override
@@ -76,7 +78,7 @@ public class BasicCharacter implements Character {
 
     @Override
     public int getHitPoints() {
-       return hitPoints;
+        return hitPoints;
     }
 
     @Override
@@ -91,76 +93,78 @@ public class BasicCharacter implements Character {
 
     @Override
     public int getLuck() {
-       return luck;
+        return luck;
     }
+
     @Override
     public int getDungeoneering() {
-       return dungeoneering;
+        return dungeoneering;
     }
 
     @Override
     public int getLockPicking() {
-       return lockPicking;
+        return lockPicking;
     }
 
     @Override
     public int getAthletics() {
-       return athletics;
+        return athletics;
     }
 
     @Override
     public int getArcana() {
-       return arcana;
+        return arcana;
     }
 
     @Override
     public int getHistory() {
-       return history;
+        return history;
     }
 
     public int getExperience() {
-       return experience;
+        return experience;
     }
 
     @Override
     public String getSpecialAbility() {
-       return specialAbility;
+        return specialAbility;
     }
 
 
 
     @Override
     public String getRacialAbility() {
-       return racialAbility;
+        return racialAbility;
     }
 
 
 
     @Override
     public String getPlayerClass() {
-       return playerClass;
+        return playerClass;
     }
 
 
     @Override
     public String getAttackType() {
-       return attackType;
+        return attackType;
     }
 
 
 
     @Override
     public String getPlayerRace() {
-       return race;
+        return race;
     }
+
     @Override
     public String getCharacterSheet() {
 
-       return String.format("======================\n" +
-               "|\tCharacter Info\n" +
-               "======================\n" +
-               "| Name: %s\n" +
-               "| Experience: %s\n", getName(), getExperience());
+        return String.format("======================\n"
+                + "|\tCharacter Info\n"
+                + "======================\n"
+                + "| Name: %s\n"
+                + "| Experience: %s\n", getName(), getExperience());
 
 
     }
