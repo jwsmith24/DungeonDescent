660ce10b75c567256119f17983fb3c687f9aae53
Jake <jwsmit24@asu.edu>
2024-02-18 07:13:19 -0600

    Rebalance monster hp

--- src/main/java/monsters/Beholder.java
+++ src/main/java/monsters/Beholder.java
@@ -7,7 +7,7 @@ public class Beholder extends Monster {
      * Builds a monster with Beholder stats.
      */
     public Beholder() {
-        super("Beholder", 140, 7, 16, 5, 10000);
+        super("Beholder", 100, 7, 16, 5, 10000);
     }
 
     /**
--- src/main/java/monsters/Giant.java
+++ src/main/java/monsters/Giant.java
@@ -3,7 +3,7 @@ package monsters;
 public class Giant extends Monster {
 
     public Giant() {
-        super("Giant", 140, 11, 18, 1, 5000);
+        super("Giant", 100, 11, 18, 1, 5000);
     }
 
     /**
