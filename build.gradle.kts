a43c6f7cfc21c5d4a898ec93e4d750e5ea8b1e09
Jake <jsmith181997@gmail.com>
2024-03-07 04:52:37 -1000

    Update existing groovy build script to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    application
+    jacoco
+
+}
+
+application {
+    mainClass.set("Main")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Use JUnit Jupiter for testing
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+
+
+
+tasks.test {
+    // use JUnit Platform for unit tests.`
+   useJUnitPlatform()
+   // run jacoco and generate report after tests are run
+   finalizedBy(tasks.jacocoTestReport)
+}
+
+
+
+jacoco {
+    toolVersion = "0.8.11"
+    reportsDirectory = layout.buildDirectory.dir("reports/jacoco")
+}
+
+// tasks.jacocoTestReport {
+//     reports {
+//         xml.required = false
+//         csv.required = false
+//         html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+//     }
+// }
+
